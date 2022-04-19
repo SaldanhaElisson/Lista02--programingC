46eaf2b7abc7d9d6ce6d658c7ed468cc16971ea3
SaldanhaElisson <elissonsaldanha19@gmail.com>
2022-04-19 19:44:55 -0300

    continue question 6 but I still finish

--- Q001-Q10/Q006.c
+++ Q001-Q10/Q006.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main(){
     // entradas
     int numeroDigitado;
-    int totalDeVotos[];
-
+    int totalDeVotos[4] = {0};
 
     // INTERFACE
     printf(" \n");
@@ -18,16 +18,53 @@ int main(){
 
     printf("QULQUER OUTRO NUMERO SERÁ CONSIDERADO NULO \n");
     printf("\n");
+  
+    // sistema de voto
+    while (1)
+    {   
 
+        printf("Digite o número do candidato para votar: \n");
+        scanf("%d", &numeroDigitado);
 
+          // se o numero for negavito vai sair do programa
+        if(numeroDigitado < 0){
+            break;
+        } else if(isalpha(numeroDigitado)){
+            break;
+        }
 
-    while (/* condition */)
-    {
-        printf("Digite o numero do seu candidato para votar: \n");
-        scanf(%d, totalDeVotos);
+        
+        switch (numeroDigitado)
+        {
+            case 7:
+                numeroDigitado = 1;
+                break;
+
+            case 5:
+                numeroDigitado = 2;
+                break;
+
+            case 0:
+                numeroDigitado = numeroDigitado;
+                break;
+            default:
+                numeroDigitado = 3;
+                break;
+        }
+
+        // BRANCO = INDICE 0
+        // RENATA = indice 1
+        // PAULO = indice 2
+        // NULO = INDICE 3;
+
+        // vai incrimetar de acordo com o votor da pessoa
+        ++totalDeVotos[numeroDigitado];
+
     }
-    
-    
+        
+
+
+        printf("%d \n", totalDeVotos[0]);
 
     return 0;
 }
