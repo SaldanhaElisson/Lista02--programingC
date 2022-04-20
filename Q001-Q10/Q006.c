7a29343fd5351eb715cd8dcd314e7ccb31efb31c
SaldanhaElisson <elissonsaldanha19@gmail.com>
2022-04-20 13:31:13 -0300

    finish questions 6

--- Q001-Q10/Q006.c
+++ Q001-Q10/Q006.c
@@ -5,6 +5,7 @@ int main(){
     // entradas
     int numeroDigitado;
     int totalDeVotos[4] = {0};
+    char validando;
 
     // INTERFACE
     printf(" \n");
@@ -20,11 +21,22 @@ int main(){
     printf("\n");
   
     // sistema de voto
-    while (1)
+    for( ; ; )
     {   
-
+        printf("\n");
         printf("Digite o número do candidato para votar: \n");
         scanf("%d", &numeroDigitado);
+
+
+        // validando voto
+        printf("\n");
+        printf("Você tem certeza? [s|n] \n");
+        validando = getchar();
+        scanf("%c", &validando);
+
+        if(validando == 'n' || validando ==! 's'){
+            continue;
+        }
 
           // se o numero for negavito vai sair do programa
         if(numeroDigitado < 0){
@@ -32,8 +44,7 @@ int main(){
         } else if(isalpha(numeroDigitado)){
             break;
         }
-
-        
+ 
         switch (numeroDigitado)
         {
             case 7:
@@ -59,12 +70,32 @@ int main(){
 
         // vai incrimetar de acordo com o votor da pessoa
         ++totalDeVotos[numeroDigitado];
-
     }
+        // processando resultados
+        // variaveis dos resultados
+        float resultados[3];
+        // indice 0 -> porcetagem dos votos em brancos
+        // indice 1 -> porcetagem dos votos da  Renata
+        // indice 2 -> porcetagem dos votos do Paulo
+
+        // calculando porcetagem dos votos do Paulo
+        resultados[2] = (float)totalDeVotos[1] / (totalDeVotos[0] + totalDeVotos [1] + totalDeVotos[2]) * 100;
+
+        // calculando porcetagem dos votos da Renata
+        resultados[1] = (float)totalDeVotos[1] / (totalDeVotos[0] + totalDeVotos [1] + totalDeVotos[2]) * 100;
         
+        // calculando porcetagem dos votos em branco
+        resultados[0] = (float)totalDeVotos[0] / (totalDeVotos[0] + totalDeVotos [1] + totalDeVotos[2]) * 100;
+
+        // imprimindo resultados
+        printf("\n");
+        printf("RESULTADOS DAS ELEIÇÕES \n");
+        printf("%.2f %% dos votos foram da Renata \n", resultados[1]);
+        printf("%.2f %% dos votos foram do Paulo \n", resultados[2]);
+        printf("%.2f %% dos votos foram em Branco \n", resultados[0]);
+        printf("TOTAL DE VOTOS: %d \n" , totalDeVotos[0] + totalDeVotos [1] + totalDeVotos[2]);
 
 
-        printf("%d \n", totalDeVotos[0]);
 
     return 0;
 }
